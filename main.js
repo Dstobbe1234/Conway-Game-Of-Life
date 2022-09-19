4050c6b5139710da5c33bff7a1cbf64a4a9fe873
David Stobbe <d.stobbe1@share.epsb.ca>
2022-09-19 16:59:06 -0600

    Done first draft

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ document.addEventListener("mousedown", mousedownListener);
 
 
 let cells = [];
+let playing = false;
 
 class cell {
     constructor(x, y, w, h) {
@@ -19,34 +20,41 @@ class cell {
         this.w = w;
         this.h = h;
         this.live = false;
-        this.neighboors = []
+        this.neighbours = []
     }
     checkNeighboors() {
-        let liveNeighboors = 0
-        for(let i = 0; i < this.neighboors; i++) {
-            if(this.neighboors[i].live) {
-                liveNeighboors++
+        this.liveNeighbours = 0
+        for(let i = 0; i < this.neighbours.length; i++) {
+            if(this.neighbours[i].live) {
+                this.liveNeighbours++
             }
         }
-        if (!this.live) {
-            if(liveNeighboors == 3) {
-                this.live = true
-            }
-        } else {
-            if(liveNeighboors < 2) {
-                this.live = false
-            } else if(liveNeighboors == 4) {
-                this.live = false
-            }
+
+    } 
+    lifeDeath() {
+        if(this.live && this.liveNeighbours === 1 || this.liveNeighbours === 0) {
+            this.live = false
+        } else if(this.live && this.liveNeighbours === 4) {
+            this.live = false;
+        } else if (!this.live && this.liveNeighbours === 3) {
+            this.live = true;
         }
+        // if (this.x === 200 && this.y === 200) {
+        //     for(let i = 0; i < this.neighbours.length; i++) {
+        //         this.neighbours[i].live = true
+        //     }
+        // }
     }
     display() {
-        ctx.fillStyle = "grey";
+        if (this.live) {
+            ctx.fillStyle = "yellow";
+        } else {
+            ctx.fillStyle = "grey";
+        }
         ctx.fillRect(this.x, this.y, this.w, this.h);
     }
 
 }
-
 function drawBackground() {
     let x = 0;
     let y = 0;
@@ -68,44 +76,72 @@ function createCells() {
             cells.push(new cell(x, y, 20, 20));
         }
     }
-
-    for(let i = 0; i < cells.length; i++) {
-            if (cells[i - 1] !== undefined) {
-                cells[i].neighboors.push(cells[i - 1])
-            }
-            if (cells[i + 1] !== undefined) {
-                cells[i].neighboors.push(cells[i + 1])
-            }
-            if (cells[i - 36] !== undefined) {
-                cells[i].neighboors.push(cells[i - 36])
-            }
-            if (cells[i + 36] !== undefined) {
-                cells[i].neighboors.push(cells[i + 36])
-            }
-    }
-    requestAnimationFrame(loop);
+    getNeighbours([-1, 1, -36, -35, -34, 34, 35, 36])
 }
+
+function getNeighbours(num) {
+    
+    for(let i = 0; i < cells.length; i++) {
+        for(let numIndex = 0; numIndex < num.length; numIndex++) {
+            if (cells[i + num[numIndex]] !== undefined) {
+                cells[i].neighbours.push(cells[i + num[numIndex]])
+            }
+        }
+    }
+    setInterval(loop, 200);
+}
+
+let mouse = {
+    x: 0,
+    y: 0,
+} 
 
 
 function mousedownListener(event) { 
-    let mouseX = event.x - cnv.getBoundingClientRect().x;
-    let mouseY = event.y - cnv.getBoundingClientRect().y;
+    mouse.down = true
+    mouse.x = event.x - cnv.getBoundingClientRect().x;
+    mouse.y = event.y - cnv.getBoundingClientRect().y;
+    placeCells()
 }
 
-
-
-function loop() {
-    for(let i = 0; i < cells.length; i++) {
-        cells[i].checkNeighboors()
-        cells[i].display();
+function placeCells() {
+    if(!playing) {
+        for(let i = 0; i < cells.length; i++) {
+            if (cells[i].x < mouse.x && mouse.x < cells[i].x + cells[i].w
+                && cells[i].y < mouse.y && mouse.y < cells[i].y + cells[i].h) {
+                if (cells[i].live) {
+                    cells[i].live = false
+                } else {
+                    cells[i].live = true
+                }
+            }
+        }
     }
-    drawBackground()
-
-    requestAnimationFrame(loop);
 }
 
 function play() {
+    if (!playing) {
+        playing = true
+    } else {
+        playing = false
+    }
+}
 
+function loop() {
+    for(let i = 0; i < cells.length; i++) {
+        if (playing) {
+            cells[i].checkNeighboors()
+        }
+    }
+    
+    for(let i = 0; i < cells.length; i++) {
+        if (playing) {
+            cells[i].lifeDeath()
+        }
+        cells[i].display();
+    }
+    drawBackground()
 }
  
+
 createCells()
